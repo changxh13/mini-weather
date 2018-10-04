b7ddb236b87a18612c9813a1e692ab8136cdb83e
changxh13 <changxh.13@sem.tsinghua.edu.cn>
2018-10-04 15:41:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# mini-weather
+This is an android project in class Software Development on Mobile Platform in School of Software and Microelectronics, PKU.
+
+The content of this project is getting the weather data and showing it in User Interface.
+
+*built in android studio 2.3.2*
+
+*Android SDK Tools 26.1.1 and API 26*
+
+
+
+
+
---
+++ app/src/main/java/com/app/kevin/miniweather/MainActivity.java
@@ -0,0 +1,18 @@
+package com.app.kevin.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by kevin on 2018/10/4.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+
+}
